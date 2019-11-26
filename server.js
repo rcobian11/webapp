269f359cb98f35b4b8302be7b6bed5f7374e989e
rcobian <rcobian29@yahoo.com>
2019-11-25 19:29:28 -0800

    fix issue

--- server.js
+++ server.js
@@ -1,12 +1,3 @@
-const express = require('express');
-const http = require('http');
-const WebSocket = require('ws');
-const path = require('path');
-const EventHubReader = require('./scripts/event-hub-reader.js');
-
-const iotHubConnectionString = "HostName=PlantMonitor.azure-devices.net;SharedAccessKeyName=service;SharedAccessKey=HA7929bOEoTSFF0v1hjWxssCT2DrjsLFyu/HTx2Wb/o=";
-const eventHubConsumerGroup = "plantdata";
-
 
 'use strict';
 
@@ -27,11 +18,12 @@ var counter = 0;
 
 
 var printMessage = function (message) {
+  counter += 1;
+  console.log(counter);
   if (message.body.waterLevel > 100) {
-    counter += 1;
     if (counter == 1){
       client.messages.create({
-        body: 'water plant pendejo Micheal Amor',
+        body: 'water plant pendejo \nMichaela ALbuja Amor',
         to: '+12096106350',  // Text this number
         from: '+12056428457' // From a valid Twilio number
       })
@@ -53,48 +45,3 @@ EventHubClient.createFromIotHubConnectionString(connectionString).then(function 
     return ehClient.receive(id, printMessage, printError, { eventPosition: EventPosition.fromEnqueuedTime(Date.now()) });
   });
 }).catch(printError);
-
-// Redirect requests to the public subdirectory to the root
-const app = express();
-app.use(express.static(path.join(__dirname, 'public')));
-app.use((req, res /* , next */) => {
-  res.redirect('/');
-});
-
-const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
-
-wss.broadcast = (data) => {
-  wss.clients.forEach((client) => {
-    if (client.readyState === WebSocket.OPEN) {
-      try {
-        console.log(`Broadcasting data ${data}`);
-        client.send(data);
-      } catch (e) {
-        console.error(e);
-      }
-    }
-  });
-};
-
-server.listen(process.env.PORT || '3000', () => {
-  console.log('Listening on %d.', server.address().port);
-});
-
-const eventHubReader = new EventHubReader(iotHubConnectionString, eventHubConsumerGroup);
-
-(async () => {
-  await eventHubReader.startReadMessage((message, date, deviceId) => {
-    try {
-      const payload = {
-        IotData: message,
-        MessageDate: date || Date.now().toISOString(),
-        DeviceId: deviceId,
-      };
-
-      wss.broadcast(JSON.stringify(payload));
-    } catch (err) {
-      console.error('Error broadcasting: [%s] from [%s].', err, message);
-    }
-  });
-})().catch();
